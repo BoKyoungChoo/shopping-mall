8c295f5c5c84655cf1821c0599bb17dbd7b871a9
chu <choo_bk@naver.com>
2023-11-23 00:24:38 +0900

    Feat

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Best</Nav.Link>
+            <Nav.Link href="#pricing">My Page</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Button variant="primary">btn</Button>
+
+      <div className="main-bg"></div>
     </div>
   );
 }
