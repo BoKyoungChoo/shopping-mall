d2daf64f2f7dca97a67ae362d20c520b704ae849
chu <choo_bk@naver.com>
2023-11-26 23:19:55 +0900

    Feat: react route 설치 및 기본 셋팅

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import "./App.css";
 import Product from "./components/product";
+import { Routes, Route, Link } from "react-router-dom";
+import Main from "./page/main";
 
 function App() {
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <Link to="/">Shop</Link>
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Best</Nav.Link>
@@ -16,32 +20,13 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
-
-      <Container>
-        <Row>
-          {productInfo.map((info) => {
-            return <Product key={info.name} productInfo={info} />;
-          })}
-        </Row>
-      </Container>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<div>상세페이지</div>} />
+        <Route path="/mypage" element={<div>마이페이지</div>} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-const productInfo = [
-  {
-    imgUrl:
-      "https://image.msscdn.net/images/goods_img/20210722/2037173/2037173_16976143519631_125.jpg",
-    name: "베이직 푸퍼",
-    price: 72910,
-  },
-  {
-    imgUrl:
-      "https://image.msscdn.net/images/goods_img/20210722/2037167/2037167_16941494688973_125.jpg",
-    name: "파스텔 푸퍼",
-    price: 52910,
-  },
-];
