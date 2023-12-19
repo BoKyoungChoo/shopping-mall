799292059bab0deaab6593a1df8942089d5778c7
chu <choo_bk@naver.com>
2023-12-19 23:48:46 +0900

    Feat: tab생성

--- src/page/detail.jsx
+++ src/page/detail.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Nav } from "react-bootstrap";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 const Detail = ({ data }) => {
@@ -16,18 +17,49 @@ const Detail = ({ data }) => {
 
   const { id } = useParams();
   const navigate = useNavigate();
+  const [tab, setTab] = useState(0);
 
   if (data.length < id || isNaN(Number(id))) return <p>없는 상품입니다.</p>;
 
   return (
-    <div>
-      {showNotice && (
-        <div className="alert alert-wraning">2초이내 구매시 할인</div>
-      )}
-      <p>{data[id].name}</p>
-      <p>{data[id].price}</p>
-      <img src={data[id].imgUrl} />
-    </div>
+    <>
+      <div>
+        {showNotice && (
+          <div className="alert alert-wraning">2초이내 구매시 할인</div>
+        )}
+        <p>{data[id].name}</p>
+        <p>{data[id].price}</p>
+        <img src={data[id].imgUrl} />
+      </div>
+      <div>
+        <Nav variant="tabs" defaultActiveKey="link0">
+          <Nav.Item
+            onClick={() => {
+              setTab(0);
+            }}
+          >
+            <Nav.Link eventKey="link0">상세</Nav.Link>
+          </Nav.Item>
+          <Nav.Item
+            onClick={() => {
+              setTab(1);
+            }}
+          >
+            <Nav.Link eventKey="link1">리뷰</Nav.Link>
+          </Nav.Item>
+          <Nav.Item
+            onClick={() => {
+              setTab(2);
+            }}
+          >
+            <Nav.Link eventKey="link2">반품 및 교환</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        {tab === 0 && <div>내용0</div>}
+        {tab === 1 && <div>내용1</div>}
+        {tab === 2 && <div>내용2</div>}
+      </div>
+    </>
   );
 };
 
